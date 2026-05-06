fc7ae1233180f4af63263ee49e7944d3fcea04ee
{PRIYANSHU <priyanshu.sinha12345@gmail.com>
2026-05-06 11:43:50 +0530

    updated

--- resources/views/adduser.blade.php
+++ resources/views/adduser.blade.php
@@ -1,24 +1,37 @@
-<pre>
-@php
-    print_r($errors->all())
-@endphp
-</pre>
-
+<!-- @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif -->
+<h1>Add User</h1>
 <form action="/adduser" method="post">
     @csrf
+
     <label>Name:</label>
-    <input type="text" name="username"><br>
+    <input type="text" name="username" value="{{ old('username') }}"><br>
+    <span class="text-danger">@error('username') {{ $message }} @enderror</span><br>
+
     <label>Email:</label>
-    <input type="email" name="useremail"><br>
+    <input type="text" name="useremail" value="{{ old('useremail') }}"><br>
+    <span class="text-danger">@error('useremail') {{ $message }} @enderror</span><br>
+
     <label>Age:</label>
-    <input type="number" name="userage"><br>
+    <input type="text" name="userage" value="{{ old('userage') }}"><br>
+    <span class="text-danger">@error('userage') {{ $message }} @enderror</span><br>
+
     <label>City:</label>
-    <select name="city"><br>
-    <option value="">--Select City--</option>
-    <option value="Delhi">Delhi</option>
-    <option value="Mumbai">Mumbai</option>
-    <option value="Chandigarh">Chandigarh</option>
-    <option value="Banglore">Banglore</option>
+    <select name="city">
+        <option value="">--Select City--</option>
+        <option value="Delhi">Delhi</option>
+        <option value="Mumbai">Mumbai</option>
+        <option value="Chandigarh">Chandigarh</option>
+        <option value="Kolkata">Kolkata</option>
     </select><br>
+    <span class="text-danger">@error('city') {{ $message }} @enderror</span><br>
+
     <button type="submit">Submit</button>
 </form>
